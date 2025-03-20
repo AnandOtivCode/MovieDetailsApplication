832684ef39e8d223af8eca92f17434179b0d42f1
Anand Otiv <w0858857@myscc.ca>
2025-03-20 18:00:55 -0400

    Initial Commit

---
+++ MovieDetailsApplication/MovieDetailsApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetailsApplicationApp.swift
+//  MovieDetailsApplication
+//
+//  Created by Anand Otiv on 2025-03-20.
+//
+
+import SwiftUI
+
+@main
+struct MovieDetailsApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
