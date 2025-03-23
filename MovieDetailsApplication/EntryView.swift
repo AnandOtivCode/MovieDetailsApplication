669cd8181541d660df4be81e5aa3e55f0b15bd36
Anand Otiv <w0858857@myscc.ca>
2025-03-22 21:58:07 -0400

    Imported the Assignment 6 developer view Working bottom tabs in entryview to navigate different pages

---
+++ MovieDetailsApplication/EntryView.swift
@@ -0,0 +1,18 @@
+//
+//  EntryView.swift
+//  MovieDetailsApplication
+//
+//  Created by Anand Otiv on 2025-03-22.
+//
+
+import SwiftUI
+
+struct EntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EntryView()
+}
--- MovieDetailsApplication/MainView.swift
+++ MovieDetailsApplication/MainView.swift
@@ -5,14 +5,60 @@
 //  Created by Anand Otiv on 2025-03-22.
 //
 
+// Picture From : https://pixabay.com/illustrations/fantasy-magic-wizard-sorcery-8803376/
+
+
 import SwiftUI
 
 struct MainView: View {
+    @State private var sheetIsShowing = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ZStack{
+                Color(.black)
+                //                .ignoresSafeArea()
+                Image("LordOfTheRingsIcon").resizable().frame(width: 400, height: 543).opacity(0.3)
+                VStack{
+                    
+                    Text("Lord Of The Rings Movies App")
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .foregroundColor(Color.green)
+                        .foregroundStyle(.white)
+                    
+                    
+                    Text("Completed by fetching data from the ITunes API")
+                        .font(.subheadline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.green)
+                        .padding(.top, 15.0)
+                    
+                }
+            }
+            
+            
+            
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Info", systemImage: "info.circle"){
+                        sheetIsShowing.toggle()
+                    }
+                }
+            }
+            .sheet(isPresented: $sheetIsShowing) {
+                VStack{
+                    Text("It's working")
+                    
+                }
+            }
+        }
     }
 }
 
+
+
+//  Button("Developer Information", systemImage: "info.circle")
 #Preview {
     MainView()
 }
