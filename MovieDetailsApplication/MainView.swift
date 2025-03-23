577d335794772362ae328207585a1ad0134eb959
Anand Otiv <w0858857@myscc.ca>
2025-03-22 21:00:00 -0400

    Started the main view

---
+++ MovieDetailsApplication/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  MovieDetailsApplication
+//
+//  Created by Anand Otiv on 2025-03-22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
